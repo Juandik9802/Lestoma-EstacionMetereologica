7417a5cf71f217aca58bee8ab1dd2760988ca229
JUAN DIEGO PACHON POVEDA <jdpachon@ucundinamarca.edu.co>
2022-07-12 10:37:14 -0500

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lestoma_EstacionMetereologica
+{
+    public partial class Estacion : Form
+    {
+        public Estacion()
+        {
+            InitializeComponent();
+        }
+
+        private void Estacion_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
